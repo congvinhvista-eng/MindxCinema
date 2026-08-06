b56b059800633e6a4284428b0cdfd75a91bd80a7
nguyen kitagawa <congvinhvista@gmail.com>
2026-08-06 22:45:39 +0700

    update

--- index.js
+++ index.js
@@ -27,3 +27,40 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "dangnhap.html";
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ss2 = document.getElementById("ss2");
+  if (!ss2) return;
+
+  ss2.addEventListener("click", () => {
+    window.location.href = "ss2-tqq.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const movie = document.getElementById("movie");
+  if (!movie) return;
+
+  movie.addEventListener("click", () => {
+    window.location.href = "movie-tqq.html";
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ss1 = document.getElementById("ss1");
+  if (!ss1) return;
+
+  ss1.addEventListener("click", () => {
+    window.location.href = "watch-tqq.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const special = document.getElementById("special");
+  if (!special) return;
+
+  special.addEventListener("click", () => {
+    window.location.href = "spc-tqq.html";
+  });
+});
